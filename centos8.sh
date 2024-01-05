33a73858c3b9f12324ac7a59cbe4c0139d20540e
vanphonguit <vanphonguit@gmail.com>
2024-01-05 15:02:07 +0700

    Update centos8.sh

--- centos8.sh
+++ centos8.sh
@@ -7,10 +7,19 @@ echo " | (__/ -_) ' \  _| (_) \__ \ / /   "
 echo "  \___\___|_||_\__|\___/|___//_/    "
 echo " OpenVPN Access Server With Unlimited License"
 
+cat >/etc/sysctl.d/openvpn.conf <<EOL
+net.ipv4.ip_forward = 1
+EOL
+systemctl disable --now firewalld
+cd /usr/local/openvpn_as/lib/python
+rm -rf __pycache__/*
+
 yum update -y
 yum install net-tools -y
 yum install ncurses-compat-libs -y
 rpm -i as-repo-centos8.rpm
+yum -y install openvpn-as
+
 rm -rf /usr/local/openvpn_as/lib/python/pyovpn-2.0-*
 cp pyovpn-2.0-py3.6.egg /usr/local/openvpn_as/lib/python/
 cd /usr/local/openvpn_as/bin
@@ -19,8 +28,6 @@ cd /usr/local/openvpn_as/bin
 
 echo "=========================DONE=============================="
 echo "SETUP ADMIN PASSWORD"
-
 passwd openvpn
-
 
 echo "=========================DONE=============================="
